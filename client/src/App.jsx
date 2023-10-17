f71ade63b79482d9025f2cc7dc40200270bbfb4e
Zac Nation <zac.nation@icloud.com>
2023-10-17 17:07:38 +0100

    Add post data + styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,76 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="p-3 max-w-2xl m-auto text-gray-600">
-      <header className="flex justify-between mb-3">
-        <a href="" className="text-bold">
+    <main className="p-3 max-w-2xl m-auto">
+      <header className="flex justify-between my-4">
+        <a href="" className="text-bold text-gray-600">
           Blog
         </a>
-        <nav className="flex gap-3">
+        <nav className="flex gap-3 text-gray-600">
           <a href="">Login</a>
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="grid grid-cols-test gap-4 mb-4">
+        <div>
+          <img
+            src="https://bazis.ca/wp-content/uploads/2020/06/skyline-sailing-summer-city-life-downtown-lifestyle-city-view-toronto-harbourfront-lake-ontario_t20_NGbYwp-scaled.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            laudantium!
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eum!
+            Debitis quis facere officiis omnis. Ex corporis dolores libero quasi
+            eius nemo aliquid autem, velit dolore, repudiandae quam, veniam
+            voluptatem?
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-test gap-4 mb-4">
+        <div>
+          <img
+            src="https://bazis.ca/wp-content/uploads/2020/06/skyline-sailing-summer-city-life-downtown-lifestyle-city-view-toronto-harbourfront-lake-ontario_t20_NGbYwp-scaled.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            laudantium!
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eum!
+            Debitis quis facere officiis omnis. Ex corporis dolores libero quasi
+            eius nemo aliquid autem, velit dolore, repudiandae quam, veniam
+            voluptatem?
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-test gap-4 mb-4">
+        <div>
+          <img
+            src="https://bazis.ca/wp-content/uploads/2020/06/skyline-sailing-summer-city-life-downtown-lifestyle-city-view-toronto-harbourfront-lake-ontario_t20_NGbYwp-scaled.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            laudantium!
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, eum!
+            Debitis quis facere officiis omnis. Ex corporis dolores libero quasi
+            eius nemo aliquid autem, velit dolore, repudiandae quam, veniam
+            voluptatem?
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
