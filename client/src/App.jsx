5a0b610a3e95b0500a1b8a8138d0ea5ffdc05793
Zac Nation <zac.nation@icloud.com>
2023-10-17 19:21:10 +0100

    Set up index route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
 import Post from "./components/Post";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <main className="p-3 max-w-2xl m-auto">
-      <Header />
-      <Post />
-    </main>
+    <Routes>
+      <Route
+        index
+        element={
+          <main className="p-3 max-w-2xl m-auto">
+            <Header />
+            <Post />
+          </main>
+        }
+      />
+    </Routes>
   );
 }
 
