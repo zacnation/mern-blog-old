c7f1d6c4ec6750962ebfaeb2c07cbaa0132dff30
Zac Nation <zac.nation@icloud.com>
2023-10-17 16:04:39 +0100

    Add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="text-3xl">test</div>;
+  return (
+    <main className="p-3 max-w-2xl m-auto text-gray-600">
+      <header className="flex justify-between mb-3">
+        <a href="" className="text-bold">
+          Blog
+        </a>
+        <nav className="flex gap-3">
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </main>
+  );
 }
 
 export default App;
