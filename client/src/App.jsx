ec35a0f6ee04571d06353f7f09bfac4558713449
Zac Nation <zac.nation@icloud.com>
2023-10-17 20:25:54 +0100

    Add login / register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import Layout from "./components/Layout";
-import Post from "./components/Post";
 import { Route, Routes } from "react-router-dom";
+import IndexPage from "./components/pages/IndexPage";
+import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Post />} />
-        <Route path="/login" element={<div>login</div>} />
-        <Route path="/register" element={<div>register</div>} />
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
