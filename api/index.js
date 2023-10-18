8beef9be04a62c3d3daa08d8344642945e9d41fa
Zac Nation <zac.nation@icloud.com>
2023-10-18 11:56:45 +0100

    Add /api + index.js

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("test ok");
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
