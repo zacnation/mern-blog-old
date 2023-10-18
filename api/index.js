ada4abe6ff154fed364ccbadfa3790538d812fc1
Zac Nation <zac.nation@icloud.com>
2023-10-18 19:43:27 +0100

    Add salt

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +17,10 @@ mongoose.connect(
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
     res.json(userDoc);
   } catch (e) {
     res.status(400).json(e);
