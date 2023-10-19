8bb6ab965ff3cc4633b8eb30a12b54d3c04e4f81
Zac Nation <zac.nation@icloud.com>
2023-10-19 16:32:56 +0100

    Conditionally render login / logout etc

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 
 export default function Header() {
+  const [username, setUsername] = useState(null);
   useEffect(() => {
-    axios.get("http://localhost:4000/profile", {
-      withCredentials: true,
-    });
+    // axios
+    //   .get("http://localhost:4000/profile", {
+    //     withCredentials: true,
+    //   })
+    //   .then((res) => {
+    //     res.json();
+    //   })
+    //   .then((userInfo) => {
+    //     setUsername(userInfo.username);
+    //   });
+
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((res) =>
+      res.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      })
+    );
   }, []);
 
   return (
@@ -15,8 +31,18 @@ export default function Header() {
         Blog
       </Link>
       <nav className="flex gap-3 text-gray-600">
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a href="/logout">Log out</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
